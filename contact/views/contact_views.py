1814aca96d8d8c7c92de9dc31c038f1e940b34f0
Daniel Jaldim <daniel.jaldim0804@gmail.com>
2024-12-11 18:40:27 -0300

    Adicionando função de busca por nomes e caracteres relacionados na agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,17 +1,71 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
+
 def index(request):
-    contacts = Contact.objects.all()
-    
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[10:20]
+
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
-    
+
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()  # Use get() corretamente
+    
+    if search_value == '':
+        return redirect('contact:index')  # Redireciona se o valor da busca estiver vazio
+
+    print(search_value)
+    
+    # Filtra os contatos com base no valor de busca
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    print(contacts.query)  # Mostra a query SQL gerada no terminal (útil para depuração)
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
